61dfd2b81a8e46ea311314b881b609228f78fdae
João Brandão <up201705573@fe.up.pt>
2019-09-23 14:52:19 +0100

    MyTriangle Primitive

--- LAIGDemoParser20192020v1.1/MySceneGraph.js
+++ LAIGDemoParser20192020v1.1/MySceneGraph.js
@@ -562,6 +562,35 @@ class MySceneGraph {
             else {
                 console.warn("To do: Parse other primitives.");
             }
+
+            if (primitiveType == 'triangle') {
+                // x1
+                var x1 = this.reader.getFloat(grandChildren[0], 'x1');
+                if (!(x1 != null && !isNaN(x1)))
+                    return "unable to parse x1 of the primitive coordinates for ID = " + primitiveId;
+
+                // y1
+                var y1 = this.reader.getFloat(grandChildren[0], 'y1');
+                if (!(y1 != null && !isNaN(y1)))
+                    return "unable to parse y1 of the primitive coordinates for ID = " + primitiveId;
+
+                // x2
+                var x2 = this.reader.getFloat(grandChildren[0], 'x2');
+                if (!(x2 != null && !isNaN(x2) && x2 > x1))
+                    return "unable to parse x2 of the primitive coordinates for ID = " + primitiveId;
+
+                // y2
+                var y2 = this.reader.getFloat(grandChildren[0], 'y2');
+                if (!(y2 != null && !isNaN(y2) && y2 > y1))
+                    return "unable to parse y2 of the primitive coordinates for ID = " + primitiveId;
+
+                var tri = new MyTriangle(this.scene, primitiveId, x1, x2, y1, y2);
+
+                this.primitives[primitiveId] = tri;
+            }
+            else {
+                console.warn("To do: Parse other primitives.");
+            }
         }
 
         this.log("Parsed primitives");
@@ -741,5 +770,6 @@ class MySceneGraph {
 
         //To test the parsing/creation of the primitives, call the display function directly
         this.primitives['demoRectangle'].display();
+        this.primitives['demoTriangle'].display();
     }
 }
