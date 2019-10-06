eaaf8bdf96b9a3f658689c8e2ac004598eb0aa96
João Brandão <up201705573@fe.up.pt>
2019-10-06 19:03:54 +0100

    Balizas e floor done

--- LAIGDemoParser20192020v1.1/MyRectangle.js
+++ LAIGDemoParser20192020v1.1/MyRectangle.js
@@ -27,7 +27,9 @@ class MyRectangle extends CGFobject {
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
-			1, 3, 2
+			1, 3, 2,
+			1, 0, 2,
+			1, 2, 3
 		];
 
 		//Facing Z positive
--- LAIGDemoParser20192020v1.1/MyTorus.js
+++ LAIGDemoParser20192020v1.1/MyTorus.js
@@ -5,95 +5,65 @@
  */
 class MyTorus extends CGFobject {
     constructor(scene, id, inner, outer, slices, loops) {
-        super(scene);
-
-        this.inner = inner;
-        this.outer = outer;
-        this.slices = slices;
-        this.loops = loops;
-        this.initBuffers();
+      super(scene);
+      this.inner = inner;
+      this.outer = outer;
+      this.slices = slices;
+      this.loops = loops;
+  
+      this.initBuffers();
     }
+  
     initBuffers() {
-        //-----declaracao-----
-        this.vertices = [];
-        this.indices = [];
-        this.normals = [];
-        this.texCoords = [];
-
-        var ang = 0;
-        var alphaAng = 2 * Math.PI / this.slices; //angulo entre as faces do prisma
-
-        var sides = 1; //nr of cylinders
-        var angRot = 0;
-        var alphaAngRot = 360/sides;
-
-        for (var j = 0; j < sides; j++) {
-
-            angRot+=alphaAngRot;//implement creation of several cylinders to create torus(see sketch)
-
-            for (var i = 0; i < this.slices; i++) {
-                var senAng = Math.sin(ang);
-                var senAng_Alpha = Math.sin(ang + alphaAng);
-                var cosAng = Math.cos(ang);
-                var cosAng_Alpha = Math.cos(ang + alphaAng);
-
-                this.vertices.push(cosAng * this.inner, 0, -senAng * this.inner);
-                this.texCoords.push(0, 1);
-                this.vertices.push(cosAng_Alpha * this.inner, 0, -senAng_Alpha * this.inner); //pontos base c/ y=0
-                this.texCoords.push(1, 1);
-
-                this.vertices.push(cosAng * this.inner, 1, -senAng * this.inner);//1 numero generico
-                this.texCoords.push(0, 0);
-                this.vertices.push(cosAng_Alpha * this.inner, 1, -senAng_Alpha * this.inner); //pontos topo c/ y=stacks
-                this.texCoords.push(1, 0);
-
-                // square normal computation
-                var normal = [
-                    cosAng,
-                    0,
-                    -senAng
-                ];
-
-                // normalization
-                var nsize = Math.sqrt(
-                    normal[0] * normal[0] +
-                    normal[1] * normal[1] +
-                    normal[2] * normal[2]
-                );
-
-                normal[0] /= nsize;
-                normal[1] /= nsize;
-                normal[2] /= nsize;
-
-                //normal values for next face(avoid using previous angle)
-                var normalNext = [
-                    Math.cos(ang + alphaAng),
-                    0,
-                    -Math.sin(ang + alphaAng)];
-
-                // push normal once for each vertex of this square
-                this.normals.push(...normal);
-                this.normals.push(...normalNext);
-                this.normals.push(...normal);
-                this.normals.push(...normalNext);
-
-                this.indices.push(4 * i, (4 * i + 1), (4 * i + 3));
-                this.indices.push(4 * i, (4 * i + 3), (4 * i + 2));
-
-
-                ang += alphaAng;
-            }
-
+      this.vertices = [];
+      this.indices = [];
+      this.normals = [];
+      this.texCoords = [];
+  
+      for (var stack = 0; stack <= this.loops; stack++) {
+        var theta = stack * 2 * Math.PI / this.loops;
+        var sinTheta = Math.sin(theta);
+        var cosTheta = Math.cos(theta);
+  
+        for (var slice = 0; slice <= this.slices; slice++) {
+          var phi = slice * 2 * Math.PI / this.slices;
+          var sinPhi = Math.sin(phi);
+          var cosPhi = Math.cos(phi);
+  
+          var x = (this.outer + (this.inner * cosTheta)) * cosPhi;
+          var y = (this.outer + (this.inner * cosTheta)) * sinPhi
+          var z = this.inner * sinTheta;
+          var s = 1 - (stack / this.loops);
+          var t = 1 - (slice / this.slices);
+  
+          this.vertices.push(x, y, z);
+          this.normals.push(x, y, z);
+          this.texCoords.push(s, t);
         }
-        this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
+      }
+  
+      for (var stack = 0; stack < this.loops; stack++) {
+        for (var slice = 0; slice < this.slices; slice++) {
+          var first = (stack * (this.slices + 1)) + slice;
+          var second = first + this.slices + 1;
+  
+          this.indices.push(first, second + 1, second);
+          this.indices.push(first, first + 1, second + 1);
+        }
+      }
+  
+      this.primitiveType = this.scene.gl.TRIANGLES;
+      this.initGLBuffers();
     }
-
-    updateBuffers(complexity) {
-        this.slices = 5 + Math.round(5 * complexity); //complexity varies 0-1, so slices varies 0-10
-
-        // reinitialize buffers
-        this.initBuffers();
-        this.initNormalVizBuffers();
+  
+    /**
+     * @method updateTexCoords
+     * Updates the list of texture coordinates of the torus
+     * @param {Array} coords - Array of texture coordinates
+     */
+    updateTexCoords(coords) {
+      this.texCoords = [...coords];
+      this.updateTexCoordsGLBuffers();
     }
-}
+  }
+  
