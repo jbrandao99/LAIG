87c2b73e58a9430b5b3b5f64d454e4efca79fdf5
João Brandão <up201705573@fe.up.pt>
2019-11-24 15:25:35 +0000

    Nurbs

---
+++ Projeto 2/LAIGDemoParser20192020v1.1/MyPatch.js
@@ -0,0 +1,29 @@
+/**
+ * MyPatch
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyPatch extends CGFobject {
+    constructor(scene, id, npointsU, npointsV, npartsU, npartsV, controlPoints) {
+        super(scene);
+        this.id = id;
+        this.npointsU = npointsU;
+        this.npointsV = npointsV;
+        this.npartsU = npartsU;
+        this.npartsV = npartsV;
+        this.controlPoints = controlPoints;
+
+        this.makeSurface(this.npointsU - 1, this.npointsV - 1, this.controlPoints);
+    }
+
+    makeSurface(degree1, degree2, controlvertexes) {
+        var nurbsSurface = new CGFnurbsSurface(degree1, degree2, controlvertexes);
+
+        this.obj = new CGFnurbsObject(
+            this.scene, this.npartsU, this.npartsV, nurbsSurface);
+    }
+
+    display() {
+        this.obj.display();
+    }
+}
